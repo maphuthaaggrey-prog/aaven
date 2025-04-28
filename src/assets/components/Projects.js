43cf9902adb02cf0ef64875699b9741bfcc8b77e
maphuthaaggrey-prog <maphuthaaggrey83@gmail.com>
2025-04-28 14:26:15 +0200

    project update

--- src/assets/components/Projects.js
+++ src/assets/components/Projects.js
@@ -1,5 +1,6 @@
 import veteranbeatx from '../images/veteranbeatx.png'
 import majitamag from '../images/majitamag.png'
+import respectmyhustle from '../images/respectmyhustle.png'
 
 export const projects = [
     {
@@ -19,6 +20,15 @@ export const projects = [
         type: "Personal Music Website",
         tools: "React Js - Javascript - CSS",
         description: `This website is a collection of music that Veteran Beatx has been part of. It showcases all the songs, albums, and projects he helped create — whether by producing, mixing, mastering, or collaborating.`,
+    },
+    {
+        id: 3,
+        project: "Respect My Hustle",
+        image: respectmyhustle,
+        link: "https://maphuthaaggrey-prog.github.io/freeze/",
+        type: "Personal Art Portfolio",
+        tools: "React Js - Javascript - CSS",
+        description: `Professional platform showcasing the creative work of a graphic designer focused on logo design, branding, and custom illustrations. The portfolio highlights Freeze’s ability to transform ideas into original visuals that capture the identity of brands, and businesses.`,
     },
 
 ]
