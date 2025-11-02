db9a2a08119b162ef3719a01c91d9627fb1b72ff
maphuthaaggrey-prog <maphuthaaggrey83@gmail.com>
2025-11-02 14:44:07 +0200

    add EMM Services project and update project details in Projects.js

--- src/assets/data/Projects.js
+++ src/assets/data/Projects.js
@@ -6,6 +6,15 @@ import emm from '../images/emm.png'
 
 export const projects = [
     {
+        id: 1,
+        project: "EMM Services",
+        image: emm,
+        link: "https://www.emm-services.co.za/",
+        type: "Business Portfolio",
+        tools: "React Js - Javascript - CSS",
+        description: `A dynamic company dedicated to providing reliable, and sustainable solutions across key sectors including industrial cleaning, and plant hire.`,
+    },
+    {
         id: 2,
         project: "Majita Mag",
         image: majitamag,
@@ -14,14 +23,16 @@ export const projects = [
         type: "Blog - Magazine",
         description: "Majita Mag is a platform designed to spotlight music, lifestyle, and entertainment. ",
     },
+
+
     {
-        id: 3,
-        project: "Veteran Beatx",
-        image: veteranbeatx,
-        link: "https://tinyurl.com/veteranbeatx",
-        type: "Personal Music Website",
+        id: 4,
+        project: "Respect My Hustle",
+        image: respectmyhustle,
+        link: "https://maphuthaaggrey-prog.github.io/freeze/",
+        type: "Personal Art Portfolio",
         tools: "React Js - Javascript - CSS",
-        description: `This website is a collection of music that Veteran Beatx has been part of. `,
+        description: `Professional platform showcasing the creative work of a graphic designer focused on logo design, branding, and custom illustrations.`,
     },
     {
         id: 5,
@@ -33,22 +44,14 @@ export const projects = [
         description: `Platform for streaming SA content creators by embedding their YouTube videos, making it easier to discover their content in one place. `,
     },
     {
-        id: 4,
-        project: "Respect My Hustle",
-        image: respectmyhustle,
-        link: "https://maphuthaaggrey-prog.github.io/freeze/",
-        type: "Personal Art Portfolio",
+        id: 3,
+        project: "Veteran Beatx",
+        image: veteranbeatx,
+        link: "https://tinyurl.com/veteranbeatx",
+        type: "Personal Music Website",
         tools: "React Js - Javascript - CSS",
-        description: `Professional platform showcasing the creative work of a graphic designer focused on logo design, branding, and custom illustrations.`,
+        description: `This website is a collection of music that Veteran Beatx has been part of. `,
     },
-    {
-        id: 1,
-        project: "EMM Services",
-        image: emm,
-        link: "https://www.emm-services.co.za/",
-        type: "Business Portfolio",
-        tools: "React Js - Javascript - CSS",
-        description: `A dynamic company dedicated to providing reliable, and sustainable solutions across key sectors including industrial cleaning, and plant hire.`,
-    },
+
 
 ]
